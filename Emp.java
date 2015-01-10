18d70f3f83ca37ef9288f692bb1e7791073c1693
Changyang <cgu5@buffalo.edu>
2015-01-10 16:43:30 -0500

    patch apply on master

--- Emp.java
+++ Emp.java
@@ -4,6 +4,9 @@ public class  Emp implements Serializable{
 	private Interger sal;
 	private String name;
 	private Date hierdate;
+	private Double sal;
+	private Double comm;
+	private Emp mgr;
 	
 	
 	
